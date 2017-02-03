ee0553c8aa329a1f17bca708293cb5f04eeeb7cf
Jayson H Bucy <jayson.h.b@gmail.com>
2017-02-02 20:31:02 -0600

    wip

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -2,10 +2,18 @@ require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
 
+  let :user { FactoryGirl.create( :user ) }
+
   describe "GET #index" do
-    it "returns http success" do
+    it "blocks unauthorized users" do
       get :index
-      expect(response).to have_http_status(:success)
+      expect(response).to redirect_to(new_user_session_path)
+    end
+
+    it "allows authorized users" do
+      sign_in(user)
+      get :index
+      expect(response).to be_success
     end
   end
 
