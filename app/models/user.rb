8b1b4d594dbdc641c65721a7c90735a763fa6c7d
Jayson H Bucy <jayson.h.b@gmail.com>
2017-02-02 22:12:42 -0600

    Checkpoint 2 adding email on user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  after_create :send_application_email
+
+  private
+  def send_application_email
+    ApplicationMailer.new_user(self).deliver_now
+  end
 end
