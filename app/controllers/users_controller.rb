221f5fead3b89d6a3b3464b3794fda87960e8655
Jayson H Bucy <jayson.h.b@gmail.com>
2017-02-27 15:47:53 -0600

    One line to rule them all

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,6 @@ class UsersController < ApplicationController
 
   private
   def set_users_wikis_to_public(user)
-    wikis = Wiki.all
-    wikis.each do |w|
-      if w.user_id === user.id
-        w.private = false
-        w.save!
-      end
-    end
+    user.wikis.update_all(private: false)
   end
 end
